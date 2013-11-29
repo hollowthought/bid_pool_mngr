77ac662dd72e9b4da9b5f820321e8a06b2181ae9
Victor Regalado <vrrs0988@gmail.com>
2013-11-29 13:46:03 -0500

    Turned the project into a dyn web app and i added the first servlet handler.

---
+++ src/com/markovLabs/servlets/PoolOperationsHandler.java
@@ -0,0 +1,23 @@
+package com.markovLabs.servlets;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/pool/*")
+public class PoolOperationsHandler extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, java.io.IOException {
+		
+	}
+}
