bc071896d7f774deac27645553ec4304f84df0c0
Victor Regalado <vrrs0988@gmail.com>
2013-11-29 16:21:03 -0500

    created basic functionality of pool servlet

--- src/com/markovLabs/servlets/PoolOperationsHandler.java
+++ src/com/markovLabs/servlets/PoolOperationsHandler.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/pool/*")
+@WebServlet("/bid_pool_mngr/*")
 public class PoolOperationsHandler extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
