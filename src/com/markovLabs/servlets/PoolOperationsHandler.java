e4d63b2f7742d42fbb56ca2ab5065107c722b1a5
Victor Regalado <vrrs0988@gmail.com>
2013-12-01 02:16:55 -0500

    Added xml file

--- src/com/markovLabs/servlets/PoolOperationsHandler.java
+++ src/com/markovLabs/servlets/PoolOperationsHandler.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/bid_pool_mngr/*")
+@WebServlet("/*")
 public class PoolOperationsHandler extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
