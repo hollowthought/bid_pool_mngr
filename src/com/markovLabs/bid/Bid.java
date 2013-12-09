71d4a5f810b2174007f231cf597d63ebdeeda0e1
Victor Regalado <vrrs0988@gmail.com>
2013-12-08 23:32:09 -0500

    Add search operation to Pool handler and new constructor for bid class so that it can be used throughout the system.

--- src/com/markovLabs/bid/Bid.java
+++ src/com/markovLabs/bid/Bid.java
@@ -1,7 +1,7 @@
 package com.markovLabs.bid;
 
 public class Bid {
-	private int id;
+	private int user_id;
 	private double bidValue=0;
 	private int bid_id;
 	
@@ -9,15 +9,21 @@ public class Bid {
 	public static final String BID_FIELD_NAME="bid";
 	public static final String BID_FIELD_NAME_VALUE = "bid_value";
 	
-	public Bid(int id,int bid_id) {
-		this.id=id;
-		this.setBid_id(bid_id);
+	public Bid(Integer id,Integer bid_id) {
+		this(id,bid_id,0.0);
 	}
-	public int getId() {
-		return id;
+	
+	public Bid(Integer user_id, Integer bid_id, Double bid_value) {
+		this.user_id=user_id;
+		this.bid_id=bid_id;
+		this.bidValue=bid_value;
 	}
-	public void setId(int id) {
-		this.id = id;
+	
+	public int getUserId() {
+		return user_id;
+	}
+	public void setUserId(int id) {
+		this.user_id = id;
 	}
 	public double getBidValue() {
 		return bidValue;
--- src/com/markovLabs/servlets/PoolOperationsHandler.java
+++ src/com/markovLabs/servlets/PoolOperationsHandler.java
@@ -15,9 +15,10 @@ import com.markovLabs.bid.Bid;
 public class PoolOperationsHandler extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private final int BUFFER_SIZE=60;
+	private final int BUFFER_SIZE=160;
 	public static final int CREATE_OP = 0;
 	public static final int DELETE_OP = 1;
+	public static final int SEARCH_OP = 2;
 
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
@@ -43,6 +44,7 @@ public class PoolOperationsHandler extends HttpServlet {
 		switch(command){
 			case PoolOperationsHandler.CREATE_OP:BidServer.bids.put(bid_id,new Bid(id,bid_id));break;
 			case PoolOperationsHandler.DELETE_OP:BidServer.bids.remove(bid_id);break;
+		//	case PoolOperationsHandler.SEARCH_OP:searchIds(bid_id);break
 		}
 	}
 }
